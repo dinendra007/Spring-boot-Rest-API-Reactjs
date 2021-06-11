fcd624a9ffa1f9cab396fef43752bb9d38336972
Atish Jain <atishj18700@gmail.com>
2021-06-11 12:11:25 +0530

    Added PUT and DELETE APIs in ProductController class

--- src/main/java/com/dinendra007/usermanagementsystem/controller/ProductController.java
+++ src/main/java/com/dinendra007/usermanagementsystem/controller/ProductController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("product/")
