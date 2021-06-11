ed82d83fb2c314ec480e5cb10d0d7a27dfc5e5e5
Atish Jain <ati18700jain@gmail.com>
2021-06-11 11:25:16 +0530

    Added PUT and Delete APIs

--- src/main/java/com/dinendra007/usermanagementsystem/controller/ProductController.java
+++ src/main/java/com/dinendra007/usermanagementsystem/controller/ProductController.java
@@ -2,7 +2,12 @@ package com.dinendra007.usermanagementsystem.controller;
 
 import com.dinendra007.usermanagementsystem.entity.Product;
 import com.dinendra007.usermanagementsystem.service.ProductService;
+import com.example.NoUserFoundException;
+import com.example.Users;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +36,24 @@ public class ProductController {
 
         return service.getProductByID(id);
     }
+    
+    @PutMapping(value="/update/{id}")
+	public Product updateData(@PathVariable("id") int id, @RequestBody final Product product)
+	{
+			Product new_product = service.findById(id).get();
+			new_product.setName(product.getName());
+			new_product.setAge(product.getAge());
+			new_product.setCity(product.getCity());
+			return service.save(new_product);
+	}
+    
+    @DeleteMapping(value="/delete/{id}")
+	public String deleteData(@PathVariable("id") int id) 
+	{
+		 service.deleteById(id);
+		 return ("deleted");
+	 }
+    
+    
+    
 }
